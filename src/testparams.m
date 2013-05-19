4b85e30a495d7013627999b11128b1a532d38e30
Aleksy Barcz <abarcz@gmail.com>
2013-05-19 18:43:37 +0200

    Removed unnecessary code

--- src/testparams.m
+++ src/testparams.m
@@ -1,7 +1,7 @@
 
-function testparams(nGnns, graph, nIterations, basename, contractionConstants, minDiffs)
+function testparams(nGnns, graph, nIterations, basename, contractionConstants, minDiffs, state)
 %
-% usage: testparams(nGnns, graph, nIterations, basename, contractionConstants, minDiffs)
+% usage: testparams(nGnns, graph, nIterations, basename, contractionConstants, minDiffs, state)
 %
 
 	assert(size(contractionConstants, 1) == 1);
@@ -10,41 +10,6 @@ function testparams(nGnns, graph, nIterations, basename, contractionConstants, m
 	for k = 1:nGnns
 		testname = strcat(basename, sprintf('_gnn%d', k));
 		gnn = initgnn(graph.maxIndegree, [5 5], [5 graph.nodeOutputSize], 'tansig');
-
-		mainFilename = strcat(testname, '.mat');
-		packedGnn = presavegnn(gnn);
-		save(mainFilename, 'packedGnn', 'graph', 'nIterations', 'contractionConstants', 'minDiffs');
-
-		nContractionConstants = size(contractionConstants, 2);
-		nMinDiffs = size(minDiffs, 2);
-		timesElapsed = zeros(nContractionConstants, nMinDiffs);
-		precisions = zeros(nContractionConstants, nMinDiffs);
-		accuracies = zeros(nContractionConstants, nMinDiffs);
-		recalls = zeros(nContractionConstants, nMinDiffs);
-		for i = 1:nContractionConstants
-			contractionConstant = contractionConstants(i);
-			gnn.contractionConstant = contractionConstant;
-			for j = 1:nMinDiffs
-				minStateDiff = minDiffs(j);
-				minErrorAccDiff = minDiffs(j);
-
-				gnn.minStateDiff = minDiffs(j);
-				gnn.minErrorAccDiff = minDiffs(j);
-				filename = strcat(testname, '_', sprintf('%d', i), '_', sprintf('%d', j), '.mat');
-
-				tic();
-				[gnn2 trainStats] = traingnn(gnn, graph, nIterations);
-				evaluation = evaluate(classifygnn(gnn2, graph), graph.expectedOutput);
-				timeElapsed = toc();
-
-				save(filename, 'trainStats', 'timeElapsed', 'evaluation', 'contractionConstant', 'minStateDiff', 'minErrorAccDiff');
-				timesElapsed(i, j) = timeElapsed;
-				precisions(i, j) = evaluation(1);
-				accuracies(i, j) = evaluation(2);
-				recalls(i, j) = evaluation(3);
-			end
-		end
-		% resave test stats with times added
-		save(mainFilename, 'packedGnn', 'graph', 'nIterations', 'contractionConstants', 'minDiffs', 'timesElapsed', 'precisions', 'accuracies', 'recalls');
+		testparamsgnn(gnn, graph, nIterations, testname, contractionConstants, minDiffs, state)
 	end
 end
