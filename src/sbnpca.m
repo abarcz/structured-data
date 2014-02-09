0949b28dd91c68e0dce7b521cea112777b146664
Aleksy Barcz <abarcz@gmail.com>
2014-02-09 21:14:50 +0100

    two types of sbllm-npca, not working

--- src/sbnpca.m
+++ src/sbnpca.m
@@ -11,28 +11,23 @@ function [pc trainedFnn msei mseo] = sbnpca(data, nComponents, nHiddenNeurons, n
 	nInputs = size(data, 2);
 	% an additional layer for calculating principal components values
 	fnn0 = initfnn(nSamples, nComponents, nHiddenNeurons, 'tansig', 'purelin');
-	fnn = initfnn(nSamples, nHiddenNeurons, nInputs);
-	fnn.weights1 = fnn0.weights2 * fnn0.weights1;	% fnn0 serves as a single layer of fnn
-	tfnn = initfnn(nComponents, nHiddenNeurons, nInputs);	% the result fnn
+	fnn = initfnn(nComponents, nHiddenNeurons, nInputs);
+	fnn.weights1 = fnn0.weights2;
 
 	t = eye(nSamples);	% inputs for fnn0
-	z = initz(fnn, t);
 	x = initz(fnn0, t);
+	z = initz(fnn, x');
 
 	msei = zeros(nIterations, 1);
 	mseo = zeros(nIterations, 1);
 	for i = 1:nIterations
-		[fnn mse q nSaturated z] = sbllm(fnn, t, data, 1, z);
-		[fnn0 mse0 q0 nSaturated0 x] = sbllm(fnn0, t, z', nInputIt, x, false);
+		[fnn mse q nSaturated z] = sbllm(fnn, x', data, 10, z);
+		[fnn0 mse0 q0 nSaturated0 x] = sbllm(fnn0, t, z', 10, x, false);
 
-		tfnn.weights1 = fnn0.weights2;
-		tfnn.bias1 = fnn0.bias2;
-		tfnn.weights2 = fnn.weights2;
-		tfnn.bias2 = fnn.bias2;
-		outputs = applynet(tfnn, fnn0.weights1');
-		msei(i) = sum(sum((check - fnn0.weights1) .^ 2));
+		outputs = applynet(fnn, fnn0.weights1');
+		%msei(i) = sum(sum((check - fnn0.weights1) .^ 2));
 		mseo(i) = sum(sum((data - outputs) .^ 2));
 	end
-	trainedFnn = tfnn;
+	trainedFnn = fnn;
 	pc = fnn0.weights1';
 end
