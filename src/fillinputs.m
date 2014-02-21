a961d76a5d2b52a0411ddb5b23c71b6c85257d1e
Aleksy Barcz <abarcz@gmail.com>
2014-02-21 13:18:13 +0100

    state initialized only in forward() fun

--- src/fillinputs.m
+++ src/fillinputs.m
@@ -3,12 +3,8 @@ function inputStruct = fillinputs(inputStruct, state=0)
 % Fill the state part of inputMatrix.
 % A source state is used multiple times in the inputMatrix.
 
-	if state == 0
-		state = initstate(inputStruct.nNodes, inputStruct.stateSize);
-	else
-		assert(size(state, 1) == inputStruct.nNodes);
-		assert(size(state, 2) == inputStruct.stateSize);
-	end
+	assert(size(state, 1) == inputStruct.nNodes);
+	assert(size(state, 2) == inputStruct.stateSize);
 	stateFirstIndex = inputStruct.stateFirstIndex;
 	for nodeIndex = 1:inputStruct.nNodes
 		nodeState = state(nodeIndex, :);
