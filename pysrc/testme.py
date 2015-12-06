149d3a2c7fbf4d9ab39838a1e9f0a7ffcbb0a8c4
Aleksy Barcz <Aleksy Barcz>
2015-12-06 21:13:40 +0100

    LM algorithm, tested on 1-5-1 architecture

--- pysrc/testme.py
+++ pysrc/testme.py
@@ -1,14 +1,28 @@
 import numpy as np
+import scipy.linalg as linalg
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+def plot_net(net, inputs, expected):
+	plt.figure(1)
+	plt.subplot(211)
+	plt.plot(inputs, expected, '.')
+	plt.subplot(212)
+	plt.plot(inputs, net.apply(inputs), '.')
+	plt.show()
 
 def testme():
 	print "test"
 	return 1
 
+def rmse(expected, outputs):
+	return np.sqrt(mean_squared_error(expected, outputs))
+
 def init_weights(nInputLines, nNeurons, factor=1):
 	if factor is None:
 		factor = nInputLines
 	# random.rand returns values distributed uniformly on [0, 1)
-	weights = np.matrix((np.random.rand(nNeurons, nInputLines) - 0.5) / factor)
+	weights = np.matrix((np.random.rand(nNeurons, nInputLines) - 0.5) / (0.5 / 0.15)) #factor)
 	return weights
 
 def mul_elementwise(a, b):
@@ -25,6 +39,8 @@ class Layer():
 	def __init__(self, nInputLines, nNeurons, activationFun):
 		if not activationFun in self.KNOWN_FUNCTIONS:
 			raise Exception('Unknown activation function: ' + str(activationFun))
+		self.nNeurons = nNeurons
+		self.nInputLines = nInputLines
 		self.weights = init_weights(nNeurons, nInputLines)
 		self.bias = init_weights(nNeurons, 1)
 		self.activationFunName = activationFun
@@ -172,5 +188,94 @@ class Net():
 		#print deltas2.inputErrors
 		deltas1 = self.hiddenLayer.calc_deltas(inputs, deltas2.inputErrors)
 		return (deltas1, deltas2)
-		
 
+	def params_per_output(self):
+		nParamsPerOutput = (self.hiddenLayer.nInputLines + 2) * self.hiddenLayer.nNeurons + 1
+		return nParamsPerOutput
+
+	"""
+	k = which output neuron to we build the jacobian for
+	"""
+	def build_jacobian(self, inputs, k):
+		if self.hiddenLayer.activationFunName != 'tansig' or self.outputLayer.activationFunName != 'purelin':
+			raise Exception("Cannot build jacobian for different arch than {tansig->purelin}")
+		hiddenOutputs = self.hiddenLayer.apply(inputs)
+		hiddenOutputsSquare = np.power(hiddenOutputs, 2)
+		nParamsPerOutput = self.params_per_output()
+		nSamples = inputs.shape[0]
+		jacobian = np.matrix(np.zeros((nSamples, nParamsPerOutput)))
+		for s in range(nSamples):
+			paramId = 0
+			# input weights
+			for i in range(self.hiddenLayer.nInputLines):
+				for j in range(self.hiddenLayer.nNeurons):
+					value = self.outputLayer.weights[j, k] * (1 - hiddenOutputsSquare[s, j]) * inputs[s, i]
+					jacobian[s, paramId] = jacobian[s, paramId] + value
+					paramId = paramId + 1
+			# input biases
+			for j in range(self.hiddenLayer.nNeurons):
+				value = self.outputLayer.weights[j, k] * (1 - hiddenOutputsSquare[s, j])
+				jacobian[s, paramId] = jacobian[s, paramId] + value
+				paramId = paramId + 1
+			# output weights
+			for j in range(self.hiddenLayer.nNeurons):
+				value = hiddenOutputsSquare[s, j]
+				jacobian[s, paramId] = jacobian[s, paramId] + value
+				paramId = paramId + 1
+			# output biases
+			value = 1
+			jacobian[s, paramId] = jacobian[s, paramId] + value
+			assert(paramId == nParamsPerOutput - 1)
+		return jacobian
+
+	def lm_get_deltas_k(self, inputs, errors, k):
+		errors = errors[:, k]
+		jacobian = self.build_jacobian(inputs, k)
+		nParamsPerOutput = self.params_per_output()
+		diag = np.sqrt(0.1) * np.matrix(np.eye(nParamsPerOutput))	# default lambda = 0.1, sqrt to be ready for LU decomposition
+		A = np.concatenate((-jacobian, diag))
+		b = np.concatenate((-errors, np.matrix(np.zeros((nParamsPerOutput, 1)))))
+		AT = np.transpose(A)
+		ATA = AT * A
+		PL, U = linalg.lu(ATA, True)	# transpose L to PL if necessary
+		PL = np.matrix(PL)	# these are arrays, which may affect further calculations
+		U = np.matrix(U)
+		z = linalg.solve(PL, AT * b)	# L * z = AT * b
+		delta = linalg.solve(U, z)	# U * delta = z
+		return delta
+
+	def lm_update_weights_k(self, deltas, k):
+		paramId = 0
+		# input weights
+		for i in range(self.hiddenLayer.nInputLines):
+			for j in range(self.hiddenLayer.nNeurons):
+				self.hiddenLayer.weights[i, j] = self.hiddenLayer.weights[i, j] + deltas[paramId]
+				paramId = paramId + 1
+		# input biases
+		for j in range(self.hiddenLayer.nNeurons):
+			self.hiddenLayer.bias[0, j] = self.hiddenLayer.bias[0, j] + deltas[paramId]
+			paramId = paramId + 1
+		# output weights
+		for j in range(self.hiddenLayer.nNeurons):
+			self.outputLayer.weights[j, k] = self.outputLayer.weights[j, k] + deltas[paramId]
+			paramId = paramId + 1
+		# output biases
+		self.outputLayer.bias[0, k] = self.outputLayer.bias[0, k] + deltas[paramId]
+		assert(paramId == self.params_per_output() - 1)
+
+	def lm_single_step(self, inputs, expected):
+		outputs = self.apply(inputs)
+		errors = expected - outputs
+		deltas = []
+		for k in range(self.outputLayer.nNeurons):
+			deltas_k = self.lm_get_deltas_k(inputs, errors, k)
+			deltas.append(deltas_k)
+		for k in range(self.outputLayer.nNeurons):
+			self.lm_update_weights_k(deltas[k], k)
+		return rmse(expected, outputs)
+
+	def lm_train(self, inputs, expected, nEpochs):
+		rmse = np.zeros(nEpochs)
+		for i in range(nEpochs):
+			rmse[i] = self.lm_single_step(inputs, expected)
+		return rmse
